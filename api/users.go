4c765833ed086c1fcf51a852cbed4d88a815ccdf
Andhika Ferdiansyah <mulyadipermadi21@gmail.com>
2024-05-15 16:30:57 +0800

    Able to get user data with go lang and fix users.go function and code structure (including date datatype handling)

--- api/users.go
+++ api/users.go
@@ -6,30 +6,66 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type UserData struct {
-	Username    string `json:"username"`
-	Email       string `json:"email"`
-	Description string `json:"description"`
-	Password    string `json:"password"`
+	Id          int       `json:"id"`
+	Username    string    `json:"username"`
+	Email       string    `json:"email"`
+	Description string    `json:"description"`
+	Password    string    `json:"password"`
+	CreatedAt   int64     `json:"createdat"`
+	Tanggal     time.Time `json:"tanggal"`
 }
 
 func main() {
 	var mux = http.NewServeMux()
 	mux.HandleFunc("/sendAllData", api_sendAllData)
+	mux.HandleFunc("/getAllData", api_getAllData)
 	fmt.Println("Server is running on port 8080")
 	http.ListenAndServe(":8080", mux)
 }
 
-func connect() (*sql.DB, error) {
-	connection, err := sql.Open("mysql", "root:@tcp(127.0.0.1)/database_kelompok")
+func connect() *sql.DB {
+	connection, err := sql.Open("mysql", "root:@tcp(127.0.0.1)/database_kelompok?parseTime=true")
 	if err != nil {
-		return nil, err
+		panic(err)
 	}
-	return connection, nil
+	return connection
+}
+
+func getData(connection *sql.DB) []UserData {
+	var userDataArray []UserData
+	data, err := connection.Query("SELECT Id, Username, Email, Description, Password, Created_at, Tanggal FROM users")
+	if err != nil {
+		panic(err)
+	} else {
+		for data.Next() {
+			var userData UserData
+			err = data.Scan(&userData.Id, &userData.Username, &userData.Email, &userData.Description, &userData.Password, &userData.CreatedAt, &userData.Tanggal)
+			if err != nil {
+				panic(err)
+			} else {
+				userDataArray = append(userDataArray, userData)
+			}
+		}
+	}
+	return userDataArray
+}
+
+func api_getAllData(w http.ResponseWriter, r *http.Request) {
+	connection := connect()
+	var userData = getData(connection)
+	dataJson, err := json.Marshal(userData)
+	if err != nil {
+		panic(err)
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		io.WriteString(w, string(dataJson))
+	}
 }
 
 func api_sendAllData(w http.ResponseWriter, r *http.Request) {
@@ -44,26 +80,17 @@ func api_sendAllData(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Error decoding JSON: "+err.Error(), http.StatusBadRequest)
 		return
 	}
-
-	connection, err := connect()
+	userData.CreatedAt = time.Now().Unix()
+	err = sendData(connect(), userData)
 	if err != nil {
-		http.Error(w, "Failed to connect to database: "+err.Error(), http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	defer connection.Close()
+	fmt.Println("Data inserted successfully (Print on terminal)")
+	io.WriteString(w, "Data inserted successfully (Print on web/postman)")
+}
 
-	_, err = connection.Exec("INSERT INTO users (Username, Email, Description, Password) VALUES (?, ?, ?, ?)",
-		userData.Username, userData.Email, userData.Description, userData.Password)
-	if err != nil {
-		http.Error(w, "Error executing SQL query: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	jsonData, err := json.Marshal(userData)
-	if err != nil {
-		http.Error(w, "Error marshalling JSON: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	io.WriteString(w, string(jsonData))
+func sendData(connection *sql.DB, user UserData) error {
+	_, err := connection.Exec("INSERT INTO users (Id,Username,Email,Description,Password,Created_at) VALUES (?,?,?,?,?,?)", user.Id, user.Username, user.Email, user.Description, user.Password, user.CreatedAt)
+	return err
 }
